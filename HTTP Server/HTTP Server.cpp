abf7713e969a0eafbbfd9a17034fd4e0d4aeeaa1
dinhchienhust1896 <dinhchienhust1896@gmail.com>
2018-05-19 16:37:53 +0700

    Ver 2

--- HTTP Server/HTTP Server.cpp
+++ HTTP Server/HTTP Server.cpp
@@ -3,7 +3,7 @@
 
 #include "stdafx.h"
 #include "HTTP Server.h"
-
+#include <direct.h>
 int main()
 {
 	int res;
@@ -98,6 +98,8 @@ DWORD WINAPI ClientThread(LPVOID lpParams)
 			return FALSE;
 		}
 		buffer[res] = 0;
+		char command[10], uri[MAX_PATH];
+		res = sscanf(buffer, "%s %s", command, uri);
 		// Check HTTP Request
 		if (strstr(buffer, " HTTP/") == NULL)
 		{
@@ -106,28 +108,29 @@ DWORD WINAPI ClientThread(LPVOID lpParams)
 		}
 
 		// Detect Method
-		if (strncmp(buffer, "GET", 3) == 0) {
+		if (strcmp(command, "GET") == 0) {
 			// GET Method
-			GET_METHOD(client, buffer);
+			GET_METHOD(client, uri);
 		}
-		else if (strncmp(buffer, "POST", 4) == 0)
+		else if (strcmp(command, "POST") == 0)
 		{
 			// POST Method
 			POST_METHOD(client, buffer);
 		}
-		else if (strncmp(buffer, "PUT", 3) == 0)
+		else if (strcmp(command, "PUT") == 0)
 		{
 			// PUT Method
 			PUT_METHOD(client, buffer);
 		}
-		else if (strncmp(buffer, "HEAD", 4) == 0)
+		else if (strcmp(command, "HEAD") == 0)
 		{
 			// HEAD Method
-			HEAD_METHOD(client, buffer);
+			HEAD_METHOD(client, uri);
 		}
-		else if (strncmp(buffer, "DELETE", 6) == 0)
+		else if (strcmp(command, "DELETE") == 0)
 		{
-			DELETE_METHOD(client, buffer);
+			// DELETE Method
+			DELETE_METHOD(client, uri);
 		}
 		else
 		{
@@ -135,151 +138,328 @@ DWORD WINAPI ClientThread(LPVOID lpParams)
 			UNKNOWN_METHOD(client, buffer);
 		}
 	}
+	printf("Close socket %d", client);
 	closesocket(client);
 	return TRUE;
 }
 
-void GET_METHOD(SOCKET client, char * request)
+void GET_METHOD(SOCKET client, char * uri)
 {
-
-	// Get URI
-	char *start = strchr(request, '/');
-	char *ptr = strstr(request, "HTTP/");
-	char uri[512]="";
-	memcpy(uri, start, ptr - start - 1);
-	// Print
-	printf("SOCKET %d : GET method. URI : %s\n", client, uri);
-	// Log
-	char msg[50];
-	sprintf(msg, "SOCKET %d : GET method!. URI : %s", client, uri);
-	Log(msg, "INFO");
-	// Send to Client
+	
+	//// Get URI
 	if (strcmp(uri, "/") == 0)
 	{
-		// Send Index.html
+		// Send list file and folder
+		// Send header
 		send(client, RESPONSE_STATUS_LINE, strlen(RESPONSE_STATUS_LINE), 0);
 		send(client, RESPONSE_CONTENT_TYPE, strlen(RESPONSE_CONTENT_TYPE), 0);
-		FILE *index = fopen("Index.html", "r");
-		if (index == NULL)
+
+		FILE *file_html = fopen("temp.html", "w");
+		fputs("<html><body>", file_html);
+		HANDLE hFind;
+		WIN32_FIND_DATAA ffd;
+		hFind = FindFirstFileA("D:\\*", &ffd);
+		if (hFind == INVALID_HANDLE_VALUE)
 		{
-			printf("Can't open file : Index.html\n");
-			SEND_404(client);
 			return;
 		}
-		fseek(index, 0, SEEK_END);
-		int size = ftell(index);
-		char msg[50] = "";
-		sprintf(msg, "Content-Length: %d\n\n", size);
-		send(client, msg, strlen(msg), 0);
-		//// Send content
-		fseek(index, 0, SEEK_SET);
-		char line[512];
-		while (fgets(line, sizeof(line), index) != NULL) {
-			// Replace \n => \r\n
-			int i = 0;
-			while (line[i] != '\0')
+		do
+		{
+			if (ffd.dwFileAttributes == FILE_ATTRIBUTE_DIRECTORY)
 			{
-				if (line[i] == '\n')
-				{
-					line[i] = '\r';
-					line[i + 1] = '\n';
-					line[i + 2] = '\0';
-					i++;
-				}
-				i++;
+				// Dir
+				char dir[MAX_PATH] = "";
+				sprintf(dir, "<a href=\"%s\">%s</a><br>", ffd.cFileName, ffd.cFileName);
+				fputs(dir, file_html);
 			}
-			send(client, line, strlen(line), 0);
+			else
+			{
+				// File
+				char dir[MAX_PATH] = "";
+				sprintf(dir, "<div>%s</div>", ffd.cFileName);
+				fputs(dir, file_html);
+			}
 		}
-		fclose(index);
-		return;
+		while (FindNextFileA(hFind, &ffd) != 0);
+		fputs("</body></html>", file_html);
+		fclose(file_html);
 	}
 	else
 	{
-		char path[MAX_PATH];
-		GetCurrentDirectoryA(MAX_PATH, path);
-		// Chang \ to /
-		int i = 0;
-		while (uri[i] != '\0')
-		{
-			if (uri[i] == '/')
-			{
-				uri[i] = '\\';
-			}
-			i++;
-		}
-		strcat(path, uri);
-		// Try open file
-		FILE *file = fopen(path, "r");
-		if (file == NULL)
-		{
-			printf("Can't open file : %s\n", path);
-			SEND_404(client);
-			return;
-		}
 
-		send(client, RESPONSE_STATUS_LINE, strlen(RESPONSE_STATUS_LINE), 0);
-		send(client, RESPONSE_CONTENT_TYPE, strlen(RESPONSE_CONTENT_TYPE), 0);
-
-		fseek(file, 0, SEEK_END);
-		int size = ftell(file);
-		char msg[50] = "";
-		sprintf(msg, "Content-Length: %d\n\n", size);
-		send(client, msg, strlen(msg), 0);
-		//// Send content
-		fseek(file, 0, SEEK_SET);
-		char line[512];
-		while (fgets(line, sizeof(line), file) != NULL) {
-			// Replace \n => \r\n
-			int i = 0;
-			while (line[i] != '\0')
-			{
-				if (line[i] == '\n')
-				{
-					line[i] = '\r';
-					line[i + 1] = '\n';
-					line[i + 2] = '\0';
-					i++;
-				}
-				i++;
-			}
-			send(client, line, strlen(line), 0);
-		}
-		fclose(file);
-		return;
 	}
+	//char *start = strchr(request, '/');
+	//char *ptr = strstr(request, "HTTP/");
+	//char uri[512]="";
+	//memcpy(uri, start, ptr - start - 1);
+	//// Print
+	//printf("SOCKET %d : GET method. URI : %s\n", client, uri);
+	//// Log
+	//char msg[50];
+	//sprintf(msg, "SOCKET %d : GET method!. URI : %s", client, uri);
+	//Log(msg, "INFO");
+	//// Send to Client
+	//if (strcmp(uri, "/") == 0)
+	//{
+	//	// Send Index.html
+	//	send(client, RESPONSE_STATUS_LINE, strlen(RESPONSE_STATUS_LINE), 0);
+	//	send(client, RESPONSE_CONTENT_TYPE, strlen(RESPONSE_CONTENT_TYPE), 0);
+	//	FILE *index = fopen("index.html", "r");
+	//	if (index == NULL)
+	//	{
+	//		printf("Can't open file : index.html\n");
+	//		send(client, RESPONSE_NOT_FOUND, strlen(RESPONSE_NOT_FOUND), 0);
+	//		return;
+	//	}
+	//	fseek(index, 0, SEEK_END);
+	//	int size = ftell(index);
+	//	char msg[50] = "";
+	//	sprintf(msg, "Content-Length: %d\n\n", size);
+	//	send(client, msg, strlen(msg), 0);
+	//	//// Send content
+	//	fseek(index, 0, SEEK_SET);
+	//	char line[512];
+	//	while (fgets(line, sizeof(line), index) != NULL) {
+	//		// Replace \n => \r\n
+	//		int i = 0;
+	//		while (line[i] != '\0')
+	//		{
+	//			if (line[i] == '\n')
+	//			{
+	//				line[i] = '\r';
+	//				line[i + 1] = '\n';
+	//				line[i + 2] = '\0';
+	//				i++;
+	//			}
+	//			i++;
+	//		}
+	//		send(client, line, strlen(line), 0);
+	//	}
+	//	fclose(index);
+	//	return;
+	//}
+	//else
+	//{
+	//	char path[MAX_PATH];
+	//	GetCurrentDirectoryA(MAX_PATH, path);
+	//	// Chang \ to /
+	//	int i = 0;
+	//	while (uri[i] != '\0')
+	//	{
+	//		if (uri[i] == '/')
+	//		{
+	//			uri[i] = '\\';
+	//		}
+	//		i++;
+	//	}
+	//	strcat(path, uri);
+	//	// Try open file
+	//	FILE *file = fopen(path, "r");
+	//	if (file == NULL)
+	//	{
+	//		printf("Can't open file : %s\n", path);
+	//		send(client, RESPONSE_NOT_FOUND, strlen(RESPONSE_NOT_FOUND), 0);
+	//		return;
+	//	}
+
+	//	send(client, RESPONSE_STATUS_LINE, strlen(RESPONSE_STATUS_LINE), 0);
+	//	send(client, RESPONSE_CONTENT_TYPE, strlen(RESPONSE_CONTENT_TYPE), 0);
+
+	//	fseek(file, 0, SEEK_END);
+	//	int size = ftell(file);
+	//	char msg[50] = "";
+	//	sprintf(msg, "Content-Length: %d\n\n", size);
+	//	send(client, msg, strlen(msg), 0);
+	//	//// Send content
+	//	fseek(file, 0, SEEK_SET);
+	//	char line[512];
+	//	while (fgets(line, sizeof(line), file) != NULL) {
+	//		// Replace \n => \r\n
+	//		int i = 0;
+	//		while (line[i] != '\0')
+	//		{
+	//			if (line[i] == '\n')
+	//			{
+	//				line[i] = '\r';
+	//				line[i + 1] = '\n';
+	//				line[i + 2] = '\0';
+	//				i++;
+	//			}
+	//			i++;
+	//		}
+	//		send(client, line, strlen(line), 0);
+	//	}
+	//	fclose(file);
+	//	return;
+	//}
 }
 void POST_METHOD(SOCKET client, char * request)
 {
 	printf("SOCKET %d : POST method\n", client);
+	// Get URI
+	char command[10], uri[MAX_PATH];
+	sscanf(request, "%s %s", command, uri);
+	// Get directory
+	char path[MAX_PATH];
+	GetCurrentDirectoryA(MAX_PATH, path);
+	// Chang \ to /
+	int i = 0;
+	while (uri[i] != '\0')
+	{
+		if (uri[i] == '/')
+		{
+			uri[i] = '\\';
+		}
+		i++;
+	}
+	strcat(path, uri);
+	// Create Folder
+	_mkdir(path);
+	// Get file name and content
+	char *begin = strstr(request, "\r\n\r\n");
+	begin += 4;
+	char filename[64];
+	char temp[64];
+	sscanf(begin, "%s\n%s", filename, temp);
+	begin += strlen(filename) + 1;
+	strcat(path, "\\");
+	strcat(path, filename);
+	FILE *file = fopen(path, "w");
+	fputs(begin, file);
+	fclose(file);
+	// Reply to client
+	send(client, RESPONSE_POST_DONE, strlen(RESPONSE_POST_DONE), 0);
+	printf("Client %d create file %s", client, filename);
 	// Log
-	char msg[50];
-	sprintf(msg, "SOCKET %d : POST method!", client);
+	char msg[MAX_PATH];
+	sprintf(msg, "SOCKET %d : POST method!. Create file %s", client, path);
 	Log(msg, "INFO");
 }
 void PUT_METHOD(SOCKET client, char * request)
 {
 	printf("SOCKET %d : PUT method\n", client);
+	// Get URI
+	char command[10], uri[MAX_PATH];
+	sscanf(request, "%s %s", command, uri);
+	// Get directory
+	char path[MAX_PATH];
+	GetCurrentDirectoryA(MAX_PATH, path);
+	// Chang \ to /
+	int i = 0;
+	while (uri[i] != '\0')
+	{
+		if (uri[i] == '/')
+		{
+			uri[i] = '\\';
+		}
+		i++;
+	}
+	strcat(path, uri);
+	char *begin = strstr(request, "\r\n\r\n");
+	begin += 4;
+	char mode[4];
+	char temp[64];
+	sscanf(begin, "%s\n%s", mode, temp);
+	begin += strlen(mode) + 1;
+	FILE *file;
+	if (strcmp(mode, "a") == 0)
+	{
+		// Apend mode
+		file = fopen(path, "a");
+		fputs(begin, file);
+		fclose(file);
+	}
+	else
+	{
+		// Write mode
+		file = fopen(path, "w");
+		fputs(begin, file);
+		fclose(file);
+	}
+	// Reply to client
+	send(client, RESPONSE_PUT_DONE, strlen(RESPONSE_PUT_DONE), 0);
+	printf("Client %d modify file %s", client, uri);
 	// Log
-	char msg[50];
-	sprintf(msg, "SOCKET %d : PUT method!", client);
+	char msg[MAX_PATH];
+	sprintf(msg, "SOCKET %d : PUT method!. File name : %s", client, path);
 	Log(msg, "INFO");
 }
-void HEAD_METHOD(SOCKET client, char * request)
+void HEAD_METHOD(SOCKET client, char * uri)
 {
 	printf("SOCKET %d : HEAD method\n", client);
+	// Get directory
+	char path[MAX_PATH];
+	GetCurrentDirectoryA(MAX_PATH, path);
+	// Chang \ to /
+	int i = 0;
+	while (uri[i] != '\0')
+	{
+		if (uri[i] == '/')
+		{
+			uri[i] = '\\';
+		}
+		i++;
+	}
+	strcat(path, uri);
+	// Try to open file
+	FILE *file = fopen(path, "r");
+	if (file != NULL)
+	{
+		// Exist file
+		send(client, RESPONSE_STATUS_LINE, strlen(RESPONSE_STATUS_LINE), 0);
+		send(client, RESPONSE_CONTENT_TYPE, strlen(RESPONSE_CONTENT_TYPE), 0);
+		send(client, "Content-length: 0\r\n\n", 20, 0);
+		return;
+	}
+	else
+	{
+		printf("File : %s not found\n", path);
+		send(client, RESPONSE_NOT_FOUND, strlen(RESPONSE_NOT_FOUND), 0);
+		return;
+	}
 	// Log
-	char msg[50];
-	sprintf(msg, "SOCKET %d : HEAD method!", client);
+	char msg[MAX_PATH];
+	sprintf(msg, "SOCKET %d : HEAD method!. File name : %s", client, path);
 	Log(msg, "INFO");
 }
-void DELETE_METHOD(SOCKET client, char * request)
+void DELETE_METHOD(SOCKET client, char * uri)
 {
 	printf("SOCKET %d : DELETE method\n", client);
+
+	// Get directory
+	char path[MAX_PATH];
+	GetCurrentDirectoryA(MAX_PATH, path);
+	// Chang \ to /
+	int i = 0;
+	while (uri[i] != '\0')
+	{
+		if (uri[i] == '/')
+		{
+			uri[i] = '\\';
+		}
+		i++;
+	}
+	strcat(path, uri);
+	// Try to Remove File
+	int status = remove(path);
+	if (status != 0)
+	{
+		printf("File : %s not found\n", path);
+		send(client, RESPONSE_NOT_FOUND, strlen(RESPONSE_NOT_FOUND), 0);
+		return;
+	}
+	else
+	{
+		printf("Delete file : %s", path);
+		// Reply to client
+		send(client, RESPONSE_DONE, strlen(RESPONSE_DONE), 0);
+	}
 	// Log
-	char msg[50];
-	sprintf(msg, "SOCKET %d : DELETE method!", client);
+	char msg[MAX_PATH];
+	sprintf(msg, "SOCKET %d : DELETE method!. File name : %s", client, path);
 	Log(msg, "INFO");
 }
+
 void UNKNOWN_METHOD(SOCKET client, char * request)
 {
 	printf("SOCKET %d : UNKNOWN method\n", client);
@@ -287,11 +467,6 @@ void UNKNOWN_METHOD(SOCKET client, char * request)
 	char msg[50];
 	sprintf(msg, "SOCKET %d : UNKNOWN method!", client);
 	Log(msg, "INFO");
-}
-
-void SEND_404(SOCKET client)
-{
-	send(client, RESPONSE_NOT_FOUND, strlen(RESPONSE_NOT_FOUND), 0);
 }
 
 void Log(char * msg, char * level)
--- HTTP Server/HTTP Server.h
+++ HTTP Server/HTTP Server.h
@@ -1,18 +1,19 @@
 #include "stdafx.h"
 
-#define SERVER_PORT 80
+#define SERVER_PORT 9000
 #define SERVER_ADDR "127.0.0.1"
 #define RESPONSE_STATUS_LINE "HTTP/1.1 200 OK\r\n"
 #define RESPONSE_CONTENT_TYPE "Content-Type: text/html; charset = utf-8\r\n"
 #define RESPONSE_NOT_FOUND "HTTP/1.1 404 Not Found\r\nContent-Type: text/html\r\nContent-length: 35\r\n\n<html><body>Not Found</body></html>"
-
+#define RESPONSE_DONE "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\nContent-length: 30\r\n\n<html><body>Done</body></html>"
+#define RESPONSE_POST_DONE "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\nContent-length: 45\r\n\n<html><body>Create file success</body></html>"
+#define RESPONSE_PUT_DONE "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\nContent-length: 45\r\n\n<html><body>Modify file success</body></html>"
 // Function
 DWORD WINAPI ClientThread(LPVOID lpParams);
-void GET_METHOD(SOCKET client, char * request);
+void GET_METHOD(SOCKET client, char * uri);
 void POST_METHOD(SOCKET client, char * request);
 void PUT_METHOD(SOCKET client, char * request);
-void HEAD_METHOD(SOCKET client, char * request);
-void DELETE_METHOD(SOCKET client, char * request);
+void HEAD_METHOD(SOCKET client, char * uri);
+void DELETE_METHOD(SOCKET client, char * uri);
 void UNKNOWN_METHOD(SOCKET client, char * request);
-void SEND_404(SOCKET client);
 void Log(char * msg, char * level);
